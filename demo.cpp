318667c4e7201c7429dce9a63cec7f8899344063
lsandra <lsandra@syr.edu>
2018-04-09 15:17:10 -0400

    make sub method 3 parameter compliance.

--- demo.cpp
+++ demo.cpp
@@ -24,8 +24,8 @@ class demoClass: demoInterface{
         return a+b+c;
     };
     
-    int sub(int a, int b){
-        return a-b;
+    int sub(int a, int b, int c){
+        return a-b-c;
     };
 
 };
